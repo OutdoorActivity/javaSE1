3e522f1bb005ce56b733ffc217956388c9d93302
OutdoorActivity <gorbachyov.fm@gmail.com>
2021-10-24 21:58:01 +0500

    Anonymous class and var testing

--- src/com/gorbachyov/javase1/Employee.java
+++ src/com/gorbachyov/javase1/Employee.java
@@ -1,8 +1,34 @@
 package com.gorbachyov.javase1;
 
 public abstract class Employee {
+    public static void main(String[] args) {
+        var employee = new Employee() {
+            private String email;
+
+            @Override
+            public void work() {
+                System.out.println("anonymous work...");
+            }
+
+            public void employeeAnonymousMethod() {
+                System.out.println("do some employee stuff...");
+            }
+
+            public String getEmail() {
+                return email;
+            }
+
+            public void setEmail(String email) {
+                this.email = email;
+            }
+        };
+        employee.employeeAnonymousMethod();
+        employee.work();
+    }
+
     private String name;
     private int id;
+
 
 
     public abstract void work();
@@ -10,7 +36,6 @@ public abstract class Employee {
     public String employeeMethod() {
         return "this is employee method";
     }
-
 
     //<editor-fold desc="get and set methods">
     public String getName() {
@@ -29,6 +54,4 @@ public abstract class Employee {
         this.id = id;
     }
     //</editor-fold>
-
-
 }
