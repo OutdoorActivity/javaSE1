4e085f1f844a617eb5c2f95906006a07381ea30b
OutdoorActivity <gorbachyov.fm@gmail.com>
2021-10-24 21:58:55 +0500

    test Stream API

--- src/com/gorbachyov/javase1/Collection.java
+++ src/com/gorbachyov/javase1/Collection.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Collection {
     public static void main(String[] args) {
@@ -18,6 +19,19 @@ public class Collection {
         for (Employee employee : employeesList) {
             System.out.println(employee);
         }
+        List<Employee> employeesList2 = new ArrayList<>();
+        employeesList2.add(new TeamLead("Б", 5));
+        employeesList2.add(Developer.developerMethod("А", 2));
+        employeesList2.add(new Trainee("С", 10));
+        employeesList2.add(new TeamLead("В", 10));
+        employeesList2.add(Developer.developerMethod("Г", 15));
+        employeesList2.add(new Trainee("Д", 10));
+
+        List<Employee> employeeList3 = employeesList2.stream().filter(employee -> employee instanceof TeamLead).collect(Collectors.toList());
+        System.out.println(employeeList3);
+        employeeList3.get(0).setName("Борис");
+        employeeList3.get(1).setName("Аркадий");
+        employeeList3.stream().map(Employee::getName).sorted(String::compareTo).forEach(System.out::println);
     }
 }
 
